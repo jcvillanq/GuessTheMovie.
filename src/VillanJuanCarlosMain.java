0e13a2a6ff14ee02bdaf097be6a83be2bb22304a
jcvillanq <55392743+jcvillanq@users.noreply.github.com>
2024-12-23 13:10:31 +0100

    init and create structure.

---
+++ src/VillanJuanCarlosMain.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class VillanJuanCarlosMain {
+    public static void main(String[] args) {
+    VillanJuanCarlosMain programa =  new VillanJuanCarlosMain();
+
+    programa.inicio();
+    programa.configuracionApp();
+    programa.bievenidaApp();
+    programa.ejecutarApp();
+    }
+
+
+    // iniciamos nuestro flujo de trabajo
+    private void inicio(){
+        System.out.println("Inicializando nuestro juego de adivinar una pelicula");
+    }
+    private void configuracionApp() {
+    }
+    private void bievenidaApp() {
+    }
+    private void ejecutarApp() {
+    }
+
+    
+    
+}
