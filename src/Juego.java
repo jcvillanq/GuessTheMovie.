853ec3632fb68835d9d49f8bdec0d4958617ee95
jcvillanq <55392743+jcvillanq@users.noreply.github.com>
2024-12-24 13:32:36 +0100

    create a new classes and put the structure.

---
+++ src/Juego.java
@@ -0,0 +1,38 @@
+
+
+import java.util.List;
+
+public class Juego {
+
+
+    private Jugador jugador;
+    private String tituloPelicula;
+    private char[] tituloMostrado;
+    private List<Character> letrasIncorrectas;
+    private int intentosRestantes;
+    private boolean juegoGanado;
+
+
+    /**
+     * Contructor que iniciara nuestro juego
+     */
+
+    public Juego() {
+        this.jugador = jugador;
+        this.letrasIncorrectas = letrasIncorrectas;
+        this.intentosRestantes = intentosRestantes;
+        this.juegoGanado = juegoGanado;
+    }
+
+
+
+
+
+
+    public void iniciar(){
+
+}
+
+
+}
+
--- src/VillanJuanCarlosMain.java
+++ src/VillanJuanCarlosMain.java
@@ -8,6 +8,8 @@ public class VillanJuanCarlosMain {
     programa.configuracionApp();
     programa.bievenidaApp();
     programa.ejecutarApp();
+    programa.despedidaApp();
+
     }
 
 
@@ -16,10 +18,21 @@ public class VillanJuanCarlosMain {
         System.out.println("Inicializando nuestro juego de adivinar una pelicula");
     }
     private void configuracionApp() {
+        System.out.println("Cargando configuracion y peliculas");
     }
     private void bievenidaApp() {
+        System.out.println("Bienvenido al juego de adivinar las peliculas");
+        System.out.println("intenta adivinar el titulo de la pelicula en 10 intentos ");
     }
+
     private void ejecutarApp() {
+        Juego juego = new Juego();
+        juego.iniciar();
+    }
+
+    private void despedidaApp(){
+
+        System.out.println("Gracias por jugar! Hasta pronto!");
     }
 
     
